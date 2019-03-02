0258a4b4d3938d9f5c799bc78015018b2604ac44
Cyril <Cyril@centra-2.local>
2019-03-03 00:51:05 +0530

    version1

--- intermediate/mongo_json_backup.py
+++ intermediate/mongo_json_backup.py
@@ -2,3 +2,21 @@
 #by @CyrilSebastian
 
 #this script checks for dbs and collections and exports in JSON format.
+
+import sys, pymongo
+from pymongo import MongoClient
+
+#lists the DBs in mongo
+client = MongoClient('localhost', 27017)
+print(client.list_database_names())
+print(client.list_collection_names())
+
+client = MongoClient('localhost', 27017)
+#db_names = client.list_database_names()
+db_name = client["pilot"]
+print(db_name.list_collection_names())
+
+db_names = client.list_database_names()
+for i in db_names:
+    sole_db = client["i"]
+    print(sole_db.list_collection_names())
